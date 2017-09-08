5fc9f0eb6993efeb4e9a7c0723cecb3535a6827d
hidapple <shotfsh@gmail.com>
2017-09-08 23:29:47 +0900

    Refactor bot

--- bot.go
+++ bot.go
@@ -10,6 +10,11 @@ import (
 var (
 	botID   string
 	botName string
+
+	commands = map[string]string{
+		"ADD":  "add",
+		"SHOW": "show",
+	}
 )
 
 type BOT struct {
@@ -34,41 +39,12 @@ func (bot *BOT) Run() int {
 			botName = ev.Info.User.Name
 
 		case *slack.MessageEvent:
-			text := ev.Text
-			channel := ev.Channel
-
-			if strings.HasPrefix(text, "<@"+botID+"> add") {
-				file, err := os.OpenFile("./idealist.txt", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-				if err != nil {
-					return 1
-				}
-				defer file.Close()
-
-				contents := strings.SplitAfter(ev.Text, "<@"+botID+"> add")
-				file.Write(([]byte)("-" + contents[1] + "\n"))
-				bot.rtm.SendMessage(bot.rtm.NewOutgoingMessage("added :+1:", channel))
+			if strings.HasPrefix(ev.Text, "<@"+botID+"> "+commands["ADD"]) {
+				add(bot, ev)
 			}
 
-			if strings.HasPrefix(text, "<@"+botID+"> show") {
-				file, err := os.Open("./idealist.txt")
-				if err != nil {
-					return 1
-				}
-				defer file.Close()
-
-				buf := make([]byte, 1024)
-				message := ""
-				for {
-					n, err := file.Read(buf)
-					if err != nil {
-						break
-					}
-					if n == 0 {
-						break
-					}
-					message += "\n" + string(buf[:n])
-				}
-				bot.rtm.SendMessage(bot.rtm.NewOutgoingMessage(message, channel))
+			if strings.HasPrefix(ev.Text, "<@"+botID+"> "+commands["SHOW"]) {
+				show(bot, ev)
 			}
 
 		case *slack.InvalidAuthEvent:
@@ -76,4 +52,32 @@ func (bot *BOT) Run() int {
 		}
 	}
 	return 0
+}
+
+func add(bot *BOT, event *slack.MessageEvent) {
+	file, _ := os.OpenFile("./idealist.txt", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	defer file.Close()
+
+	contents := strings.SplitAfter(event.Text, "<@"+botID+"> add")
+	file.Write(([]byte)("-" + contents[1] + "\n"))
+	bot.rtm.SendMessage(bot.rtm.NewOutgoingMessage("added :+1:", event.Channel))
+}
+
+func show(bot *BOT, event *slack.MessageEvent) {
+	file, _ := os.Open("./idealist.txt")
+	defer file.Close()
+
+	buf := make([]byte, 1024)
+	message := ""
+	for {
+		n, err := file.Read(buf)
+		if err != nil {
+			break
+		}
+		if n == 0 {
+			break
+		}
+		message += "\n" + string(buf[:n])
+	}
+	bot.rtm.SendMessage(bot.rtm.NewOutgoingMessage(message, event.Channel))
 }
